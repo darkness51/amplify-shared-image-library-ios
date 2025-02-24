4918a20933e6a7725dd1cd020ff8fd87c2cbfef6
Carlos Aguilar <caguilar@dwdandsolutions.com>
2025-02-24 09:34:24 -0500

    Initial Commit

---
+++ SharedImageGallery/SharedImageGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharedImageGalleryApp.swift
+//  SharedImageGallery
+//
+//  Created by Carlos Aguilar on 2/24/25.
+//
+
+import SwiftUI
+
+@main
+struct SharedImageGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
